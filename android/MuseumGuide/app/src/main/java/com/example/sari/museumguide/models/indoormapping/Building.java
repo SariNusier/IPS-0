e1cd238aff1a0c6e7a4555d8328c62a53e695049
Sari Nusier <sari@sqhive.london>
2016-02-15 16:46:27 +0000

    Added some models.

---
+++ android/MuseumGuide/app/src/main/java/com/example/sari/museumguide/models/indoormapping/Building.java
@@ -0,0 +1,7 @@
+package com.example.sari.museumguide.models.indoormapping;
+
+/**
+ * Created by sari on 15/02/16.
+ */
+public class Building {
+}
---
+++ android/MuseumGuide/app/src/main/java/com/example/sari/museumguide/models/indoormapping/Room.java
@@ -0,0 +1,14 @@
+package com.example.sari.museumguide.models.indoormapping;
+
+import com.example.sari.museumguide.tools.Rectangle;
+
+public class Room {
+    private String roomName;
+    private Rectangle roomRectangle;
+    private String roomDescription;
+
+
+    public Room(){
+
+    }
+}
