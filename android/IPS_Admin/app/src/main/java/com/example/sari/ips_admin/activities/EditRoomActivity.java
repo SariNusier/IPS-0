e380ea1884c3f26758c01a0cdb29131c9d5a59b4
Sari Nusier <sari.nusier@kcl.ac.uk>
2016-03-28 15:39:35 +0100

    Added start/stop button when measuring room.

--- android/IPS_Admin/app/src/main/java/com/example/sari/ips_admin/activities/EditRoomActivity.java
+++ android/IPS_Admin/app/src/main/java/com/example/sari/ips_admin/activities/EditRoomActivity.java
@@ -16,17 +16,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sari.ips_admin.R;
 import com.example.sari.ips_admin.database.Database;
 import com.example.sari.ips_admin.models.positioning.RPMeasurement;
 
 import java.util.List;
-
+//TODO: Add start and stop measurement.
 public class EditRoomActivity extends AppCompatActivity {
     private String currentRoom = "";
     WifiManager wifiManager;
     private TextView resultsTextview;
+    private boolean toMeasure = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,26 +36,23 @@ public class EditRoomActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         resultsTextview = (TextView) findViewById(R.id.edit_room_results_textview);
+        Button measureButton = (Button) findViewById(R.id.room_measure_button);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         Intent intent = getIntent();
         currentRoom = intent.getStringExtra("room_id");
-        Button measureButton = (Button) findViewById(R.id.room_measure_button);
-        measureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                measureRoom();
-            }
-        });
         wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        List<ScanResult> found = wifiManager.getScanResults();
-        wifiManager.startScan();
         registerReceiver(broadcastReceiver,new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
+        measureButton.setOnClickListener(startMListener);
 
     }
 
-    private void measureRoom(){
+    private void startMeasurement(){
+        wifiManager.startScan();
+        toMeasure = true;
+    }
 
+    private void stopMeasurement(){
+        toMeasure = false;
     }
 
     @Override
@@ -61,18 +60,43 @@ public class EditRoomActivity extends AppCompatActivity {
         super.onDestroy();
         unregisterReceiver(broadcastReceiver);
     }
+    private View.OnClickListener startMListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ((Button)v).setText("STOP");
+            startMeasurement();
+            v.setOnClickListener(stopMListener);
+        }
+    };
+
+    private View.OnClickListener stopMListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ((Button)v).setText("MEASURE");
+            stopMeasurement();
+            v.setOnClickListener(startMListener);
+        }
+    };
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            List<ScanResult> found = wifiManager.getScanResults();
-            resultsTextview.setText("");
-            for(ScanResult sr : found){
-                Log.d("FOUND: ", ""+sr.BSSID+": "+sr.level);
-                resultsTextview.append(sr.BSSID + " = " + sr.level + "\n");
-                Database.postMeasurement(new RPMeasurement(sr.BSSID,currentRoom,sr.level));
+            if (toMeasure) {
+                List<ScanResult> found = wifiManager.getScanResults();
+                resultsTextview.setText("");
+                String[] RPIDs = new String[found.size()];
+                Double[] values = new Double[found.size()];
+
+                for (ScanResult sr : found) {
+                    Log.d("FOUND: ", "" + sr.BSSID + ": " + sr.level);
+                    resultsTextview.append(sr.BSSID + " = " + sr.level + "\n");
+                    RPIDs[found.indexOf(sr)] = sr.BSSID;
+                    values[found.indexOf(sr)] = (double) sr.level;
+                }
+                Database.postMeasurement(new RPMeasurement(RPIDs, values, currentRoom));
+                Toast.makeText(getApplicationContext(),"SENT",Toast.LENGTH_SHORT).show();
+                wifiManager.startScan();
             }
-            wifiManager.startScan();
         }
     };
 }
