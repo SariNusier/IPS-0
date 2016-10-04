402a82c1cd589cb778a8cf642a34e5b12b1bb01d
Sari Nusier <sari@sqhive.london>
2016-10-04 14:15:56 +0100

    Moved activity to new package.

--- android/MuseumGuide/app/src/main/java/org/example/museumguide/activities/MainActivity.java
+++ android/MuseumGuide/app/src/main/java/org/example/museumguide/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package org.example.museumguide;
+package org.example.museumguide.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.example.museumguide.R;
 
 public class MainActivity extends AppCompatActivity {
 
