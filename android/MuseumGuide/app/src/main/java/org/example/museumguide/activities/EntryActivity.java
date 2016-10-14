c63a081207e59b198d7ee981d5c7bf78c7391e86
SerbanAvram <serban_p.t_avram@yahoo.com>
2016-10-14 19:10:16 +0300

    Added EntryActivity with XML

---
+++ android/MuseumGuide/app/src/main/java/org/example/museumguide/activities/EntryActivity.java
@@ -0,0 +1,15 @@
+package org.example.museumguide.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.example.museumguide.R;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+}
